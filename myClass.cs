0ae2285b11ce9ab551c4263d4b9cd2ba3e653ca9
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-04-01 12:55:01 +0200

    Revert "Delete myClass.cs"
    This reverts commit a38877022a9de1268172be394aef1fb2425811eb.

---
+++ myClass.cs
@@ -0,0 +1,15 @@
+namespace GitExercise
+{
+    class myClass
+    {
+        public string navn { get; set; }
+        public string adresse { get; set; }
+
+        public string By { get; set; }
+    
+    public myClass(string navn )
+    {
+        this.navn = navn;
+    }
+    }
+}
