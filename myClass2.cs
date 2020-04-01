bd845678ac7ab7669f15202e41e176943a6e0156
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-04-01 12:48:46 +0200

    Create myClass2.cs
    tilføjet en ny klasse  Myclass2

---
+++ myClass2.cs
@@ -0,0 +1,15 @@
+namespace GitExercise
+{
+    class myClass2
+    {
+        public string navn { get; set; }
+        public string adresse { get; set; }
+
+        public string By { get; set; }
+    
+    public myClass2(string navn )
+    {
+        this.navn = navn;
+    }
+    }
+}
